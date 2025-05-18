ff72d10cf6475a0078df2c63310ce1fcc668c369
Baptiste Bouillon <1bouillonbaptiste@gmail.com>
2025-05-18 12:57:51 +0200

    feat: create `Fluctuations`.

---
+++ ptahlmud/entities/fluctuations.py
@@ -0,0 +1,18 @@
+"""Define a `Fluctuations` entity.
+
+Financial time-series are often represented as candle collections, as known as **fluctuations**.
+"""
+
+from pydantic import BaseModel, ConfigDict
+
+from ptahlmud.types.candle import Candle
+from ptahlmud.types.period import Period
+
+
+class Fluctuations(BaseModel):
+    """Represent financial fluctuations."""
+
+    config = ConfigDict(arbitrary_types_allowed=True)
+
+    candles: list[Candle]
+    period: Period
