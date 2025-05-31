b8b5dda6da646d2378b7f47f3044c146eb0069f9
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2025-05-31 16:35:41 +0200

    feat: implement `Portfolio._perform_entry()` method.
    This method is the entry action performed when processing a trade.
    The opposite action is `Portfolio._perform_exit()` that do the reversed operation.
    I finally ended up creating `WealthSeries` to manage the wealth of a portfolio.
    This creates a lot of code; it looks like cleared when the logic is not gathered in a single place, though.

--- ptahlmud/backtesting/portfolio.py
+++ ptahlmud/backtesting/portfolio.py
@@ -12,7 +12,7 @@ from decimal import Decimal
 from ptahlmud.types.signal import Action
 
 
-@dataclass
+@dataclass(slots=True)
 class WealthItem:
     """Represent the wealth of a portfolio at a given time.
 
@@ -23,29 +23,106 @@ class WealthItem:
     asset: Decimal
     currency: Decimal
 
+    def __post_init__(self):
+        if (self.asset < 0) | (self.currency < 0):
+            raise ValueError("Cannot store negative amount of asset or currency.")
+
+    def add_currency(self, amount: float) -> None:
+        """Update `currency`."""
+        if self.currency + Decimal(amount) < 0:
+            raise ValueError("Cannot store negative amount of currency.")
+        self.currency += Decimal(amount)
+
+    def add_asset(self, volume: float) -> None:
+        """Update `asset`."""
+        if self.asset + Decimal(volume) < 0:
+            raise ValueError("Cannot store negative volume of asset.")
+        self.asset += Decimal(volume)
+
+
+@dataclass(slots=True)
+class TimedAction:
+    """An action that occurred at a specific time."""
+
+    date: datetime
+    action: Action
+
 
 @dataclass
 class WealthSeries:
     """Store the wealth of a portfolio over time.
 
+    Items and signals are always ordered by date.
+
     Attributes:
-        items: wealth time series values as a list
-        actions: list of actions that have been applied to create the series
+        items: wealth time series values
+        actions: list of signals that have been applied to create the series
     """
 
     items: list[WealthItem]
-    actions: list[Action]
+    actions: list[TimedAction]
+
+    def entries_after(self, date: datetime) -> bool:
+        """Check if there are any entries after a given date."""
+        for _action in reversed(self.actions):
+            if _action.date < date:
+                # since signals are ordered, the following stored signals occurred before
+                return False
+            if _action.action == Action.ENTER:
+                return True
+        return False
+
+    def get_currency_at(self, date: datetime) -> float:
+        """Return the money free to be invested."""
+        item_index = _find_date_position(date=date, date_collection=[item.date for item in self.items]) - 1
+        return float(self.items[item_index].currency)
+
+    def get_asset_at(self, date: datetime) -> float:
+        """Return the money free to be invested."""
+        item_index = _find_date_position(date=date, date_collection=[item.date for item in self.items]) - 1
+        return float(self.items[item_index].asset)
+
+    def _new_entry(self, date: datetime) -> None:
+        """Create a new timed entry in the series."""
+        if date < self.items[0].date:
+            raise ValueError("Cannot enter the market before the initial date.")
+        new_action = TimedAction(date=date, action=Action.ENTER)
+        current_dates = [action.date for action in self.actions]
+        self.actions.insert(_find_date_position(date, current_dates), new_action)
+
+    def _update_wealth(self, date: datetime, investment: float, volume: float) -> None:
+        """Update the wealth series."""
+        before_item_index = _find_date_position(date, [item.date for item in self.items]) - 1
+        before_item = self.items[before_item_index]
+        new_item = WealthItem(
+            date=date, asset=before_item.asset + Decimal(volume), currency=before_item.currency - Decimal(investment)
+        )
+
+        # if any following items, update them too
+        for _item in self.items[before_item_index + 1 :]:
+            _item.add_currency(-investment)  # investing equals removing currency
+            _item.add_asset(volume)
+        self.items.insert(before_item_index + 1, new_item)
+
+    def invest(self, date: datetime, investment: float, volume: float) -> None:
+        """Put money in the market, receive a certain volume of an asset."""
+        self._new_entry(date)
+        self._update_wealth(date, investment, volume)
+
+
+def _find_date_position(date: datetime, date_collection: list[datetime]) -> int:
+    """Find the index to insert the input date in the ordered collection."""
+    for index, date_i in enumerate(reversed(date_collection)):
+        if date >= date_i:
+            return len(date_collection) - index
+    return 0
 
 
 class Portfolio:
     """Represent a trading portfolio.
 
-    A portfolio is the volume of an asset and amount of currency detained.
-    Each action on the market is recorded and triggers an update of the wealth.
-
-    Note: the portfolio manages a single asset. To deal with multiple coins, you can wrap the items
-    in a new `WealthSeries` (or whatever name) class that is assigned to an asset.
-    The current class would be responsible for updating the series corresponding to a traded asset.
+    The portfolio expects to be updated with trades ordered by date.
+    It cannot process trades that are older than the last processed trade.
 
     Args:
         starting_date: date when the series begins
@@ -64,3 +141,21 @@ class Portfolio:
             )
         ]
         self.wealth_series = WealthSeries(items=wealth_items, actions=[])
+
+    def _perform_entry(self, date: datetime, investment: float, volume: float) -> None:
+        """Enter the market if the portfolio wealth allows it."""
+        if self.wealth_series.entries_after(date):
+            raise ValueError("Cannot enter the market before an existing entry.")
+
+        if self.wealth_series.get_currency_at(date) < investment:
+            raise ValueError("Not enough capital to enter the market.")
+
+        self.wealth_series.invest(date, investment, volume)
+
+    def get_available_capital_at(self, date: datetime) -> float:
+        """Return the money free to be invested."""
+        return self.wealth_series.get_currency_at(date)
+
+    def get_asset_volume_at(self, date: datetime) -> float:
+        """Return the money free to be invested."""
+        return self.wealth_series.get_asset_at(date)
--- tests/backtesting/test_portfolio.py
+++ tests/backtesting/test_portfolio.py
@@ -1,6 +1,28 @@
 from datetime import datetime
+from decimal import Decimal
 
-from ptahlmud.backtesting.portfolio import Portfolio
+import pytest
+
+from ptahlmud.backtesting.portfolio import Portfolio, TimedAction, WealthItem, WealthSeries
+from ptahlmud.types.signal import Action
+
+
+def test_wealth_series_update_wealth_after_date():
+    """Check items after the new item date are updated."""
+    wealth_series = WealthSeries(
+        items=[
+            WealthItem(date=datetime(2020, 1, 1), asset=Decimal(2), currency=Decimal(80)),
+            WealthItem(date=datetime(2020, 1, 3), asset=Decimal(1), currency=Decimal(90)),
+        ],
+        actions=[TimedAction(date=datetime(2020, 1, 3), action=Action.EXIT)],
+    )
+    wealth_series.invest(date=datetime(2020, 1, 2), investment=-10, volume=-1)
+
+    last_item = wealth_series.items[-1]
+
+    assert last_item.date == datetime(2020, 1, 3)
+    assert last_item.asset == Decimal(0)
+    assert last_item.currency == Decimal(100)
 
 
 def test_portfolio():
@@ -12,3 +34,43 @@ def test_portfolio():
     assert wealth_item.date == datetime(2020, 1, 1)
     assert wealth_item.asset == 0
     assert wealth_item.currency == 100
+
+
+def test__perform_entry():
+    portfolio = Portfolio(starting_date=datetime(2020, 1, 1), starting_asset=0, starting_currency=100)
+
+    portfolio._perform_entry(datetime(2020, 1, 2), 10, 1)
+    assert portfolio.get_available_capital_at(datetime(2020, 1, 2)) == 90
+    assert portfolio.get_asset_volume_at(datetime(2020, 1, 2)) == 1
+
+    portfolio._perform_entry(datetime(2020, 1, 3), 20, 1)
+    assert portfolio.get_available_capital_at(datetime(2020, 1, 3)) == 70
+    assert portfolio.get_asset_volume_at(datetime(2020, 1, 3)) == 2
+
+    portfolio._perform_entry(datetime(2020, 1, 4), -30, 1)
+    assert portfolio.get_available_capital_at(datetime(2020, 1, 4)) == 100
+    assert portfolio.get_asset_volume_at(datetime(2020, 1, 4)) == 3
+
+
+def test__perform_entry_fails_before_start_date():
+    portfolio = Portfolio(starting_date=datetime(2020, 1, 1), starting_asset=0, starting_currency=100)
+
+    with pytest.raises(ValueError, match="Cannot enter the market before the initial date."):
+        portfolio._perform_entry(datetime(2019, 12, 31), 10, 1)
+
+
+def test__perform_entry_fails_existing_entry_before():
+    portfolio = Portfolio(starting_date=datetime(2020, 1, 1), starting_asset=0, starting_currency=100)
+
+    portfolio._perform_entry(datetime(2020, 1, 2), 10, 1)
+    assert portfolio.get_available_capital_at(datetime(2020, 1, 2)) == 90
+
+    with pytest.raises(ValueError, match="Cannot enter the market before an existing entry."):
+        portfolio._perform_entry(datetime(2020, 1, 1), 10, 1)
+
+
+def test__perform_entry_fails_low_capital():
+    portfolio = Portfolio(starting_date=datetime(2020, 1, 1), starting_asset=0, starting_currency=100)
+
+    with pytest.raises(ValueError, match="Not enough capital to enter the market."):
+        portfolio._perform_entry(datetime(2020, 1, 1), 110, 1)
