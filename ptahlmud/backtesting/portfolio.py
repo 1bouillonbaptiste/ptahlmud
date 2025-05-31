b203f3c44645d4f710741a2116f96e9e7fcbda7a
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2025-05-31 12:52:01 +0200

    feat: access `Portfolio` hidden state.

--- ptahlmud/backtesting/portfolio.py
+++ ptahlmud/backtesting/portfolio.py
@@ -9,10 +9,12 @@ from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
 
+from ptahlmud.types.signal import Action
+
 
 @dataclass
 class WealthItem:
-    """Represent the state of a portfolio at a given time.
+    """Represent the wealth of a portfolio at a given time.
 
     Because we manage money and volumes, we use decimals to avoid rounding errors.
     """
@@ -22,10 +24,24 @@ class WealthItem:
     currency: Decimal
 
 
+@dataclass
+class WealthSeries:
+    """Store the wealth of a portfolio over time.
+
+    Attributes:
+        items: wealth time series values as a list
+        actions: list of actions that have been applied to create the series
+    """
+
+    items: list[WealthItem]
+    actions: list[Action]
+
+
 class Portfolio:
     """Represent a trading portfolio.
 
     A portfolio is the volume of an asset and amount of currency detained.
+    Each action on the market is recorded and triggers an update of the wealth.
 
     Note: the portfolio manages a single asset. To deal with multiple coins, you can wrap the items
     in a new `WealthSeries` (or whatever name) class that is assigned to an asset.
@@ -37,13 +53,14 @@ class Portfolio:
         starting_currency: available currency at the start of the series
     """
 
-    _items: list[WealthItem]
+    wealth_series: WealthSeries
 
     def __init__(self, starting_date: datetime, starting_asset: float, starting_currency: float):
-        self._items = [
+        wealth_items = [
             WealthItem(
                 date=starting_date,
                 asset=Decimal(starting_asset),
                 currency=Decimal(starting_currency),
             )
         ]
+        self.wealth_series = WealthSeries(items=wealth_items, actions=[])
--- tests/backtesting/test_portfolio.py
+++ tests/backtesting/test_portfolio.py
@@ -5,4 +5,10 @@ from ptahlmud.backtesting.portfolio import Portfolio
 
 def test_portfolio():
     portfolio = Portfolio(starting_date=datetime(2020, 1, 1), starting_asset=0, starting_currency=100)
-    assert portfolio is not None
+
+    assert len(portfolio.wealth_series.items) == 1
+
+    wealth_item = portfolio.wealth_series.items[0]
+    assert wealth_item.date == datetime(2020, 1, 1)
+    assert wealth_item.asset == 0
+    assert wealth_item.currency == 100
