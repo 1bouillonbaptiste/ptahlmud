aa6ec6006cc5a9ca8c78920f9b234875070055a9
Baptiste BOUILLON <72198879+1bouillonbaptiste@users.noreply.github.com>
2025-06-12 12:21:34 +0200

    :recycle: Expose public classes and functions (#10)

--- ptahlmud/backtesting/__init__.py
+++ ptahlmud/backtesting/__init__.py
@@ -2,3 +2,9 @@
 
 The backtest is a simulation of the market. We calculate the trade that would have occurred given entry signals.
 """
+
+from ptahlmud.backtesting.backtest import process_signals
+from ptahlmud.backtesting.portfolio import Portfolio
+from ptahlmud.backtesting.position import Trade
+
+__all__ = ["Portfolio", "Trade", "process_signals"]
